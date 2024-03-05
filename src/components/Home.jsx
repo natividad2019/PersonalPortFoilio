949fc305d4ac41a3ef6486cc8fb2aa4facfe6bd4
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-03-05 15:19:36 -0700

    added a image and the discription about myself

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import HeroImage from "../assets/heroImage.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I am a Full Stack Developer
+          </h2>
+          <p>
+            As a recent grad, I love full-stack development and have two years'
+            experience. Right now, I'm into building web apps with React,
+            Tailwind, Next.js, and SQL.
+          </p>
+          <div>
+            <button>
+              Portfolio
+              <span>
+                <MdOutlineKeyboardArrowRight />
+              </span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            src={HeroImage}
+            alt="my profile"
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
