540c771994dea22ad61c56f615b6a4af626f3691
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-03-05 12:33:24 -0700

    added the some more new futures on the navabar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const NavBar = () => {
+  return (
+    <div className="flex justify-between items-center w-full h-20 text-white bg-black fixed">
+      <div>
+        <h1>Vinny</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
