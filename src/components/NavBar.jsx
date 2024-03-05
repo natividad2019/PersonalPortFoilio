a2620eef85f9765d41926f3eab2dc8c996dfdced
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-03-05 12:48:14 -0700

    add navibars

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,16 @@ import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-between items-center w-full h-20 text-white bg-black fixed">
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-        <h1>Vinny</h1>
+        <h1 className="text-5xl font-signature ml-2">Vinny</h1>
       </div>
+
+      <ul className="flex">
+        <li className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200">Home</li>
+        
+       
+      </ul>
     </div>
   );
 };
