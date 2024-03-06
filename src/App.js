7624a0873088acc310417882fb78776c7d4b9f6f
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-03-06 12:45:14 -0700

    I added the about section about myself

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import About from "./components/About"; // Import the About component
 import SocialLinks from "./components/SocialLinks";
 
 function App() {
@@ -7,6 +9,7 @@ function App() {
     <div>
       <NavBar />
       <Home />
+      <About />
       <SocialLinks />
     </div>
   );
---
+++ src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div>
+        <div>
+          <p>About</p>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde
+          consectetur qui, laudantium odit nesciunt hic labore explicabo nisi
+          alias, adipisci suscipit? Maiores labore accusantium rem. Nesciunt
+          dolorum sint at vel dolor soluta, magnam minus nulla earum aspernatur
+          quam quod animi eius magni aliquam eos iure pariatur molestias error
+          facilis.
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+          delectus, facilis labore consequuntur sit deleniti pariatur, ab qui
+          nihil explicabo repellat doloribus odio repudiandae natus, facere
+          dolores tempora nobis fuga ipsa reprehenderit praesentium sed laborum.
+          Id suscipit in facere, corporis praesentium eius delectus laborum
+          dolor, ab illum repudiandae similique blanditiis?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
