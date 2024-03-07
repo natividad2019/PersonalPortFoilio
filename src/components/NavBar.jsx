507f6f5c1eabf4988bef13f4d36fadb4bccd7b5f
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-03-07 12:33:48 -0700

    I add other sections and I modified a few things

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-scroll";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
+
   const links = [
     {
       id: 1,
@@ -25,6 +27,7 @@ const NavBar = () => {
       link: "contact",
     },
   ];
+
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
@@ -37,12 +40,11 @@ const NavBar = () => {
             key={id}
             className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
           >
-            {link}
+            <Link to={link} smooth duration={500}>
+              {link}
+            </Link>
           </li>
         ))}
-        <li className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200">
-          Home
-        </li>
       </ul>
 
       <div
@@ -59,7 +61,14 @@ const NavBar = () => {
               key={id}
               className="px-4 cursor-pointer capitalize py-6 text-4xl"
             >
-              {link}
+              <Link
+                onClick={() => setNav(!nav)}
+                to={link}
+                smooth
+                duration={500}
+              >
+                {link}
+              </Link>
             </li>
           ))}
         </ul>
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -31,7 +31,7 @@ const SocialLinks = () => {
           Mail <HiOutlineMail size={30} />
         </>
       ),
-      href: "mailto:foo@gmail.com",
+      href: "https://mail.google.com/mail/u/1/#inbox",
     },
     {
       id: 4,
